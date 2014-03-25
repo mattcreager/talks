87f7d7c2a806827c3a8eb27efff556d22141d404
Matt Creager <mattcreager@goinstant.com>
2014-03-25 16:00:07 -0300

    Final version

--- angular-intro/js/main.js
+++ angular-intro/js/main.js
@@ -3,45 +3,24 @@
 
 'use strict';
 
-(function($, Reveal) {
-  Reveal.addEventListener('slidechanged', function(e) {
-    var slideNumber = Reveal.getIndices().h;
+angular
+  .module('AngularIntro', [])
+  .controller('SectionTitleCtrl', ['$scope', function($scope) {
+    $scope.section = {}
 
-    if (slideNumber === 29) {
-      setInterval(itBeRainbowTime, 200);
-    }
+    $scope.$watch(function() {
+      return Reveal.getCurrentSlide().getAttribute('data-title');
+    }, function(newVal, oldVal) {
+      $scope.section.title = newVal;
+    });
 
-    var previousTitle = null;
+    Reveal.addEventListener('slidechanged', $scope.$apply.bind($scope));
 
-    if (e.previousSlide) {
-      previousTitle = $(e.previousSlide).data('title');
-    }
-
-    var currentTitle = $(e.currentSlide).data('title');
-
-    if (!currentTitle) {
-      $('.df-frame').hide();
-      return;
-    }
-    if (previousTitle === currentTitle) return;
-
-    $('.df-frame').show();
-    $('#section-title').text(currentTitle);
-  });
-
-  function itBeRainbowTime() {
-    $('.df-bar').css('background-color', getRandomColor());
-    $('#section-title').css('color', getRandomColor());
-  }
-
-  function getRandomColor() {
-      var letters = '0123456789ABCDEF'.split('');
-      var color = '#';
-      for (var i = 0; i < 6; i++ ) {
-          color += letters[Math.round(Math.random() * 15)];
-      }
-      return color;
-  }
-})(jQuery, Reveal);
-
-
+    // function updateTitle() {
+    //   var title = Reveal.getCurrentSlide().getAttribute('data-title');
+    //   console.log(title);
+    //   $scope.$apply(function() {
+    //     $scope.section.title = title;
+    //   });
+    // }
+  }]);
