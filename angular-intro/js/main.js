62ad74aa09aa7cc21c2f230c5c4b6318febd5ecd
Matt Creager <mattcreager@goinstant.com>
2014-04-12 07:40:23 -0700

    Moar fixes

--- angular-intro/js/main.js
+++ angular-intro/js/main.js
@@ -6,21 +6,15 @@
 angular
   .module('AngularIntro', [])
   .controller('SectionTitleCtrl', ['$scope', function($scope) {
-    $scope.section = {}
+    $scope.section = {};
 
-    $scope.$watch(function() {
-      return Reveal.getCurrentSlide().getAttribute('data-title');
-    }, function(newVal, oldVal) {
-      $scope.section.title = newVal;
-    });
+    Reveal.addEventListener('slidechanged', updateTitle);
 
-    Reveal.addEventListener('slidechanged', $scope.$apply.bind($scope));
+    function updateTitle() {
+      var title = Reveal.getCurrentSlide().getAttribute('data-title');
 
-    // function updateTitle() {
-    //   var title = Reveal.getCurrentSlide().getAttribute('data-title');
-    //   console.log(title);
-    //   $scope.$apply(function() {
-    //     $scope.section.title = title;
-    //   });
-    // }
+      $scope.$apply(function() {
+        $scope.section.title = title;
+      });
+    }
   }]);
